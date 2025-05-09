48022c2145e607e9375c5d25ac6e9ba3ccbb0e8f
Menaal-26 <261935148@formanite.fccollege.edu.pk>
2025-05-09 23:35:14 +0500

    Commit #7 Video #4
    Inventory created, currently using color rect in place of actual inventory to make sure its working correctly

--- script/player.gd
+++ script/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 var speed = 100
 var player_state
-
+@export var inv: Inv
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("left", "right", "up", "down")
 	if direction.x ==0 and direction.y==0:
